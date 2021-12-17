4a2b8b56c5e72d70a8b17d83ea1c78242ce2d1d9
jianchengliang <liangjc@amethystum.com>
2021-12-17 22:18:54 +0800

    remove demo

--- demo/pythontest/apitest.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-data = {
-    "name":"edsion",
-    "age":18,
-    "job":"worker"
-}
-res = requests.post("https://wb.deallate.site/api/d2312a53a71e6d5aa047fcc726bac21f43a54ac38c0dc76ef52697cac23452cd8b57d58ca7d9c7558a543d91c8515f0d/c8e6db78aa158000b638875000063405f9f2cd27214b07045e3aa8e0faf7fa03fc10ea92520059c97d33f4ca12cf32ec/f2406042916a2398d03f212e729ec90e289dcf03dceaf822ecef5309aad78c21d9af4cf1bd44eb7d2cc06b4112e2738d/a",data=data)
-print(res.text)
--- demo/pythontest/userid/index.py
+++
@@ -1,8 +0,0 @@
-
-
-def main(event):
-    print(event["key1"],event["key2"])
-    return event["key1"]+event["key2"]
-
-def test():
-    return "hello"
